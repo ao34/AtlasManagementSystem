957c88bde3373f23a3a69c96ccf184994a6dca20
ao34 <s4.jack2434@gmail.com>
2022-12-08 09:28:27 +0900

    seed

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -12,6 +12,11 @@ class SubjectsTableSeeder extends Seeder
      */
     public function run()
     {
+         DB::table('subjects')->insert([
+            ['subject' => '国語'],
+            ['subject' => '数学'],
+            ['subject' => '英語'],
+        ]);
         // 国語、数学、英語を追加
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,29 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'over_name' => '青木',
+            'under_name' => '早穂',
+            'over_name_kana' => 'アオキ',
+            'under_name_kana' => 'サホ',
+            'mail_address' => 'aoki@mail.com',
+            'sex' => '2',
+            'birth_day' => '99990101',
+            'role' => '1',
+            'password' => bcrypt('test1234'),
+        ]);
+
+         DB::table('users')->insert([
+            'over_name' => '山田',
+            'under_name' => '太郎',
+            'over_name_kana' => 'ヤマダ',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 't@mail.com',
+            'sex' => '1',
+            'birth_day' => '99990101',
+            'role' => '4',
+            'password' => bcrypt('12345678'),
+         ]);
 
     }
 }
