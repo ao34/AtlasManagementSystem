7561164ef53d739537a61b51dd47db5292d985a7
ao34 <s4.jack2434@gmail.com>
2023-01-14 20:15:55 +0900

    名前の変更

---
+++ database/migrations/2022_03_23_123654_create_subjects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement()->comment('id');
+            $table->string('subject')->comment('科目');
+            $table->timestamp('created_at')->nullable()->comment('登録日時');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
