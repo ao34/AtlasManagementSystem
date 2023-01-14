6fd84757b093e71ac8bba668365a9a6234554f0c
ao34 <s4.jack2434@gmail.com>
2023-01-14 12:13:04 +0900

    バリデーション修正

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -200,7 +200,7 @@
         <div class="mt-3">
           <label class="d-block m-0" style="font-size:13px">確認用パスワード</label>
           <div class="border-bottom border-primary">
-            <input type="password" class="border-0 w-100 password_confirmation" name="password">
+            <input type="password" class="border-0 w-100 password_confirmation" name="password_confirmation">
           </div>
         </div>
         <div class="mt-5 text-right">
